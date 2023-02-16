35a00774ac2a8d2273bb4eca2d5d658033434178
nghia124 <104367982+nghia124@users.noreply.github.com>
2023-02-16 11:04:58 +0700

    home page updated

--- resources/views/components/books/book-item.blade.php
+++ resources/views/components/books/book-item.blade.php
@@ -1,4 +1,4 @@
-<div class="col-3 py-3 game">
+{{-- <div class="col-3 py-3 game">
     <img src="images/book/{{ $book['img'] }}" alt="" class="card-img-top game-img center-img">
     <div class="card-body text-center">
         <h5 class='card-title'>{{ $book['book_name'] }}</h5>
@@ -14,6 +14,35 @@
             <a href="{{ route('detail', $book['book_id']) }}" class="btn btn-primary col-12">
                 Thông tin chi tiết
             </a>
+        </div>
+    </div>
+</div> --}}
+
+<div class="col-3 py-3 mb-3 game">
+    <div class="card h-100 border-0">
+        <img class="card-img-top game-img center-img" src="images/book/{{ $book['img'] }}" alt="..." />
+        <div class="card-body">
+            <div class="text-center">
+                <p class="">{{ $book['book_name'] }}</p>
+            </div>
+            <div class="text-center d-flex justify-content-center align-items-end">
+                <h5 class="text-danger ">{{ $book['price'] }} đ</h5>
+            </div>
+        </div>
+        <div class="card-footer border-top-0 bg-transparent">
+            {{-- <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div> --}}
+            <div class="align-text-bottom">
+                <div>
+                    <a href="" class="btn btn-outline-primary col-12">
+                        <i class="fa-solid fa-cart-shopping fa-lg"></i> Mua ngay
+                    </a>
+                </div>
+                <div class="pt-2">
+                    <a href="{{ route('detail', $book['book_id']) }}" class="btn btn-primary col-12">
+                        Thông tin chi tiết
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,7 +33,7 @@
     </div>
 
     <div class="container">
-        <div class="row">
+        <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
             @foreach ($books as $item)
                 <x-books.book-item :book="$item" />
             @endforeach
